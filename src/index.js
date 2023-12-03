738c5187a0eb80ad6a162fe5d597e59c54bb6634
Tahsin Ismail <2547855+tahsinismail@users.noreply.github.com>
2023-12-03 23:51:06 +0600

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import BookingPage from "./components/BookingPage";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Order from "./components/Order";
+import Reservations from "./components/Reservations";
+import Cart from "./components/Cart";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "menu",
+    element: <Menu />,
+  },
+  {
+    path: "order",
+    element: <Order />,
+  },
+  {
+    path: "reservations",
+    element: <Reservations />,
+  },
+  {
+    path: "cart",
+    element: <Cart />,
+  },
+  {
+    path: "booking",
+    element: <BookingPage />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
 
