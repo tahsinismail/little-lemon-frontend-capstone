1562199bb95a0dde3324d0ed4016581bbf87051b
Tahsin Ismail <2547855+tahsinismail@users.noreply.github.com>
2023-12-07 00:12:27 +0600

    applied some semantic tags

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "./Hero";
-
+import Featured from "./Featured";
+import ShopLocation from "./ShopLocation";
 const Main = () => {
   return (
     <main>
       <Hero />
+      <Featured />
+      <ShopLocation />
     </main>
   );
 };
