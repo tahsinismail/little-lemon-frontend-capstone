153f4a518970945de51cb03a84c1ce5a5837b73f
Tahsin Ismail <2547855+tahsinismail@users.noreply.github.com>
2023-12-06 16:49:50 +0600

    some page improvements

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComingSoon = () => {
+  return (
+    <div className="w-[95%] m-auto h-96 bg-[#495e57] rounded-2xl flex justify-center items-center text-2xl text-[#f4ce14] font-semibold">
+      Coming Soon!
+    </div>
+  );
+};
+
+export default ComingSoon;
---
+++ src/components/NoPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoPage = () => {
+  return (
+    <div className="w-full h-96 bg-[#495e57] rounded-2xl flex flex-col justify-center items-center ">
+      <h1 className="text-2xl text-[#f4ce14] font-semibold">404!</h1>
+      <p className="text-white font-light">No Page found.</p>
+    </div>
+  );
+};
+
+export default NoPage;
